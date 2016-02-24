6e108292d33ca078f9b1e6c43b5588acfd3eea7f
Vitaly _Vi Shukela <vi0oss@gmail.com>
2016-02-24 20:08:01 +0300

    WiP

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 extern crate fuse;
 extern crate time;
+extern crate nix;
 
 use std::env;
 use std::path::Path;
 use time::Timespec;
 use fuse::{FileType, FileAttr, Filesystem, Request, ReplyData, ReplyEntry, ReplyAttr, ReplyDirectory};
 
+use nix::sys::signal;
+
+extern fn handle_sigint(_:i32) {
+    let outstanding_blocks = 0;
+    println!("Throwing away {} dirty blocks.", outstanding_blocks);
+    ::std::process::exit(0);
+}
 
 fn main () {
     let argv = env::args_os();
@@ -19,13 +27,13 @@ fn main () {
         println!("Example session (approximate):");
         println!(" 1$ dd if=/dev/zero bs=4096 count=2560 of=sf.dat");
         println!(" 1$ touch mp.dat");
-        println!(" 1$ outoforderfs sf.dat mp.fat 4096 10000 100");
+        println!(" 1$ outoforderfs sf.dat mp.dat 4096 10000 100");
         println!(" 1$ (switch to a new tab)");
         println!(" 2$   mkfs mp.dat");
         println!(" 2$   mount mp.dat -o loop mnt");
         println!(" 2$   start using mnt (filling with logs, creating sqlite, etc)");
         println!(" 1$ ^C");
-        println!(" 1$ outoforderfs sf.dat mp.fat 4096 10000 100");
+        println!(" 1$ outoforderfs sf.dat mp.dat 4096 10000 100");
         println!(" 2$   umount mnt");
         println!(" 2$   mount mp.dat -o loop mnt");
         println!(" 2$   inspect mnt for breakage, incomplete/zeroed files, etc");
@@ -39,4 +47,12 @@ fn main () {
     let maxtime         = env::args_os().nth(4).unwrap();
     let maxblocks       = env::args_os().nth(5).unwrap();
     //fuse::mount(OutoforderFs, &mountpoint, &[]);
+    
+    
+    let sig_action = signal::SigAction::new(handle_sigint,
+                                          signal::SockFlag::empty(),
+                                          signal::SigSet::empty());
+    unsafe { signal::sigaction(signal::SIGINT, &sig_action); }
+    
+    ::std::thread::sleep_ms(1000);
 }
