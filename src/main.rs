861c7b26d08f90e4b4b0885f4cdc23014c78a4b5
Vitaly _Vi Shukela <vi0oss@gmail.com>
2016-02-24 21:42:56 +0300

    data structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+#![feature(time2)]
+
+#![allow(unused)]
+#![warn(unused_must_use)]
+
 extern crate fuse;
 extern crate time;
 extern crate nix;
@@ -6,8 +11,55 @@ use std::env;
 use std::path::Path;
 use time::Timespec;
 use fuse::{FileType, FileAttr, Filesystem, Request, ReplyData, ReplyEntry, ReplyAttr, ReplyDirectory};
+use std::time::Instant;
+use std::time::Duration;
 
 use nix::sys::signal;
+
+use std::sync::{Condvar, Mutex, MutexGuard};
+
+type BlockIndex = u64;
+
+#[derive(Eq,PartialEq)]
+struct DelayedWriteback {
+    to_be_written_at : Instant,
+    block_index : BlockIndex,
+}
+
+type BlockCache = std::collections::BTreeMap<BlockIndex, Vec<u64>>;
+type Queue = std::collections::BinaryHeap<DelayedWriteback>;
+
+struct State {
+    cache: BlockCache,
+    queue: Queue,
+    attention_of_writeback_thread: Condvar,
+}
+
+impl Default for State { fn default() -> Self { State {
+        cache: Default::default(),
+        queue: Default::default(),
+        attention_of_writeback_thread: Condvar::new(), // https://github.com/rust-lang/rust/issues/31865
+    } } }
+
+
+impl Ord for DelayedWriteback {
+    fn cmp(&self, peer:&DelayedWriteback) -> ::std::cmp::Ordering {
+        use std::cmp::Ordering::{Greater,Less,Equal};
+        // flipped ordering for the time
+        let o1 = peer.to_be_written_at.cmp(&self.to_be_written_at);
+        if o1 != Equal { return o1; }
+        
+        // fall-back ordering by index
+        return self.block_index.cmp(&peer.block_index);
+    }
+}
+impl PartialOrd for DelayedWriteback {
+    fn partial_cmp(&self, other: &DelayedWriteback) -> Option<::std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+
 
 extern fn handle_sigint(_:i32) {
     let outstanding_blocks = 0;
@@ -39,7 +91,6 @@ fn main () {
         println!(" 2$   inspect mnt for breakage, incomplete/zeroed files, etc");
         
         ::std::process::exit(1);
-        return;
     }
     let source_file     = env::args_os().nth(1).unwrap();
     let mountpoint_file = env::args_os().nth(2).unwrap();
@@ -52,7 +103,7 @@ fn main () {
     let sig_action = signal::SigAction::new(handle_sigint,
                                           signal::SockFlag::empty(),
                                           signal::SigSet::empty());
-    unsafe { signal::sigaction(signal::SIGINT, &sig_action); }
+    unsafe { signal::sigaction(signal::SIGINT, &sig_action).unwrap(); }
     
-    ::std::thread::sleep_ms(1000);
+    ::std::thread::sleep(Duration::from_secs(1));
 }
